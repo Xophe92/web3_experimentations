069859740f30cace082a660395bf375971103267
Xophe92 <bonnefoy.christophe@gmail.com>
2022-04-05 23:37:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from "react"
+import {ethers} from "ethers"
+
 function App() {
+
+
+  const [state, setState] = useState({accounts:[], connected:false});
+
+
+  async function connectAccounts(){
+    if (window.ethereum){
+      const accounts = await window.ethereum.request({
+        method:"eth_requestAccounts"
+      })
+
+      setState({accounts})
+    }
+
+  }
+
+  useEffect(
+    ()=> {connectAccounts();},
+     []
+  );
+
+  async function connect(){
+
+      const message = "signez en bas svp"
+      const signature = await window.ethereum.request({
+        method:"personal_sign", 
+        params: [message, state["accounts"][0]]
+      })
+      const signatureAddr = await ethers.utils.verifyMessage(message, signature)
+      if (signatureAddr.toUpperCase() != state["accounts"][0].toUpperCase()){
+        console.log("Who are you !", signatureAddr, state["accounts"][0])
+      } else {
+          // Of Course to be performed on the server side !!
+          setState({...state, connected:true})
+      }
+      return(signature)
+
+  }
+
+  async function disconnect(){
+
+      setState({...state, connected:false})
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        state["connected"] ?
+        <div>Connected as {state["accounts"][0]} <button onClick={disconnect}>Disconnect</button></div> :
+        <button onClick={connect}>Log in !</button>
+      }
     </div>
   );
 }
